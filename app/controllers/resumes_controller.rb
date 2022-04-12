f10e88e19299ba10ff3e4ab014106b63b00ac21e
zoe <chaonbie@gmail.com>
2022-04-12 16:12:25 +0800

    CRUD finished

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,10 +1,14 @@
 class ResumesController < ApplicationController
+    before_action :find_resume, only: [:show, :destroy, :edit, :update]
+    
     def index
         @resumes = Resume.all
     end
+    
     def new
         @resume = Resume.new
     end
+    
     def create
         @resume = Resume.new(resume_params)
 
@@ -15,8 +19,32 @@ class ResumesController < ApplicationController
             render action: :new #這邊是借new.erb那個檔案來渲染 並非走回第五行
         end
     end
+    
+    def show
+    end
+    
+    def destroy
+        @resume.destroy
+        redirect_to resumes_path, notice: "已刪除"
+    end
+
+    def edit
+    end
+
+    def update
+        if @resume.update(resume_params)
+            redirect_to resumes_path, notice: "修改成功"
+        else
+            render action: :edit #這邊是借edit.erb那個檔案來渲染
+        end
+    end
+
     private
     def resume_params
         params.require(:resume).permit(:title, :content, :status)
+    end
+
+    def find_resume
+        @resume = Resume.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/resumes", to: "resumes#index"
-  get "/resumes/new", to: "resumes#new", as: "new_resume"
-  post "/resumes", to: "resumes#create"
+  resources :resumes
+  # 上面這行等於下面八行
+  # get "/resumes", to: "resumes#index"
+  # get "/resumes/new", to: "resumes#new", as: "new_resume"
+  # post "/resumes", to: "resumes#create"
+  # get "/resumes/:id", to: "resumes#show", as: "resume"
+  # delete "/resumes/:id", to: "resumes#destroy"
+  # get "/resumes/:id/edit", to: "resumes#edit", as: "edit_resume"
+  # patch "/resumes/:id", to: "resumes#update"
+  # put "/resumes/:id", to: "resumes#update"
 
-  root "pages#home"
+  root "resumes#index"
 end
